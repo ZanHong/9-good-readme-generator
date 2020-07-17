72e5d364f5284e49fbfc65eee87e8d635c8baf07
Zan Hong <mokzanhong92@hotmail.com>
2020-07-17 22:51:08 +0930

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,79 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const markDownGenerator = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "A brief description of your project",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "How to install your project?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "How to use your project?",
+        name: "usage"
+    },
+    {
+        type: "list",
+        message: "What is the license used for your project?",
+        name: "license",
+        choices: [
+            "MIT",
+            "GNU",
+            "Unlicensed"
+        ]
+    },
+    {
+        type: "input",
+        message: "How to contribute to your project?",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "How to test your code",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "What is your username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// function to initialize program
+async function init() {
+    try {
+        inquirer
+            .prompt(questions)
+            .then(function ({ title, description, installation, usage, license, contribution, test, username, email }) {
+                const readme = markDownGenerator(data);
+            })
+    } catch (err) {
+        console.log(err);
+    }
+}
+// function call to initialize program
+init();
